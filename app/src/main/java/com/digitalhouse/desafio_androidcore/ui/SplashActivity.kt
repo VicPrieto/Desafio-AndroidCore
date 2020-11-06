78793962694436127025390bf79b7539897dff2a
Victória Gamarano Prieto <viqprieto@gmail.com>
2020-11-06 12:35:10 -0300

    first commit

---
+++ app/src/main/java/com/digitalhouse/desafio_androidcore/ui/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.digitalhouse.desafio_androidcore.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.digitalhouse.desafio_androidcore.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, 5000)
+    }
+}
